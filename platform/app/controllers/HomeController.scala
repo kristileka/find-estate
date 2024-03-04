eadbcf14676362653f3ecd1fc2f73bdc9ad92797
Kristi Leka <m.kristileka@gmail.com>
2024-03-04 22:51:42 +0100

    setting up

--- platform/app/controllers/HomeController.scala
+++ platform/app/controllers/HomeController.scala
@@ -8,14 +8,8 @@ import javax.inject._
   * application's home page.
   */
 @Singleton
-class HomeController @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  /** Create an Action to render an HTML page.
-    *
-    * The configuration in the `routes` file means that this method
-    * will be called when the application receives a `GET` request with
-    * a path of `/`.
-    */
   def test(): Action[AnyContent] = Action {
     Ok("Asda")
   }
