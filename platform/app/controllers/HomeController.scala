0b771f8a1c92390a8c691aa481dddcc3b004a93b
Kristi Leka <m.kristileka@gmail.com>
2024-03-25 23:48:46 +0100

    setting up

--- platform/app/controllers/HomeController.scala
+++ platform/app/controllers/HomeController.scala
@@ -6,10 +6,10 @@ import javax.inject._
 
 /** This controller creates an `Action` to handle HTTP requests to the
   * application's home page.
+
   */
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-
   def test(): Action[AnyContent] = Action {
     Ok("test controller")
   }
